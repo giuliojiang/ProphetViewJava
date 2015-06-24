eb1302cd201358f9061688c04518c45568f3815e
Giulio Jiang <gj414@ic.ac.uk>
2015-06-24 11:57:53 +0100

    added normalization, dot and cross to Vec3

--- src/prophetView/geometry/Vec3.java
+++ src/prophetView/geometry/Vec3.java
@@ -1,54 +1,112 @@
 package prophetView.geometry;
 
-public class Vec3<T extends Number>
+public class Vec3
 {
 
-    private T x;
-    private T y;
-    private T z;
+    private double x;
+    private double y;
+    private double z;
+    
+    public Vec3()
+    {
+        x = 0;
+        y = 0;
+        z = 0;
+    }
 
-    public Vec3(T u)
+    public Vec3(double u)
     {
         x = u;
         y = u;
         z = u;
     }
 
-    public Vec3(T x, T y, T z)
+    public Vec3(double x, double y, double z)
     {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    public T getX()
+    public double getX()
     {
         return x;
     }
 
-    public void setX(T x)
+    public void setX(double x)
     {
         this.x = x;
     }
 
-    public T getY()
+    public double getY()
     {
         return y;
     }
 
-    public void setY(T y)
+    public void setY(double y)
     {
         this.y = y;
     }
 
-    public T getZ()
+    public double getZ()
     {
         return z;
     }
 
-    public void setZ(T z)
+    public void setZ(double z)
     {
         this.z = z;
+    }
+
+    public double length()
+    {
+        return Math.sqrt(x*x + y*y + z*z);
+    }
+    
+    public void setValues(double x, double y, double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    
+    public void divideByScalar(double d)
+    {
+        x /= d;
+        y /= d;
+        z /= d;
+    }
+    
+    public Vec3 getNormalized()
+    {
+        double len = dot(this);
+        if (len <= 0)
+        {
+            return this;
+        }
+        double invLen = 1 / Math.sqrt(len);
+        return new Vec3(x*invLen, y*invLen, z*invLen);
+    }
+    
+    public double dot(Vec3 other)
+    {
+        return this.x * other.x + this.y * other.y + this.z * other.z;
+    }
+    
+    public Vec3 cross(Vec3 b)
+    {
+        Vec3 a = this;
+        
+        return new Vec3(
+                a.y * b.z - a.z * b.y,
+                a.z * b.x - a.x * b.z,
+                a.x * b.y - a.y * b.x);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "x=" + x + ",y=" + y + ",z=" + z;
     }
 
     // ############################################
@@ -58,12 +116,15 @@ public class Vec3<T extends Number>
     {
         System.out.println("Vec3 tests");
 
-        Vec3<Double> v1 = new Vec3<Double>(new Double(1.1), new Double(2.9),
-                new Double(3.5));
+        Vec3 v1 = new Vec3(1.1,2.9,3.65);
 
         System.out.println("x=" + v1.getX());
         System.out.println("y=" + v1.getY());
         System.out.println("z=" + v1.getZ());
+        
+        System.out.println("length=" + v1.length());
+        
+        System.out.println("normalized:" + v1.getNormalized());
     }
 
 }
