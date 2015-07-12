dc9b7ded562902954c6f40e162eff878cea4802e
Giulio Jiang <gj414@ic.ac.uk>
2015-07-12 20:11:20 +0100

    initialized integrator and directLighting integrator skeleton files

---
+++ src/prophetView/integrator/Integrator.java
@@ -0,0 +1,12 @@
+package prophetView.integrator;
+
+import java.awt.Image;
+
+public interface Integrator
+{
+
+    public void render();
+    
+    public Image getImage();
+    
+}
---
+++ src/prophetView/integrator/directLighting/DirectLighting.java
@@ -0,0 +1,24 @@
+package prophetView.integrator.directLighting;
+
+import java.awt.Image;
+
+import prophetView.integrator.Integrator;
+
+public class DirectLighting implements Integrator
+{
+
+    @Override
+    public void render()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Image getImage()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
