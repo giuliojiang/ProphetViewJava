52fd5965d5477692cc70ef347d7038ede95f628c
Giulio Jiang <gj414@ic.ac.uk>
2015-07-15 19:03:45 +0100

    moved out intersection result from Sphere class to generic Tuple class

---
+++ src/prophetView/utils/Tuple.java
@@ -0,0 +1,14 @@
+package prophetView.utils;
+
+public class Tuple<A, B>
+{
+    public A a;
+    public B b;
+
+    public Tuple(A a, B b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+}
